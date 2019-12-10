9d58e4a106d44d9c7ea38173f8170c8655429316
Nuno Amaral <nuno.amaral@academiadecodigo.org>
2019-12-10 01:33:27 -0100

    step6 done

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -67,5 +67,68 @@ namespace StarChart.Controllers
 
             return Ok(celestialObjects);
         }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] CelestialObject celestialObject)
+        {
+            _context.CelestialObjects.Add(celestialObject);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetById", new { id = celestialObject.Id }, celestialObject);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] CelestialObject celestialObject)
+        {
+            var objectToUpdate = _context.CelestialObjects.Find(id);
+
+            if(objectToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            objectToUpdate.Name = celestialObject.Name;
+            objectToUpdate.OrbitalPeriod = celestialObject.OrbitalPeriod;
+            objectToUpdate.OrbitedObjectId = celestialObject.OrbitedObjectId;
+
+            _context.CelestialObjects.Update(objectToUpdate);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpPatch("{id}/{name}")]
+        public IActionResult RenameObject(int id, string name)
+        {
+            var objectToUpdate = _context.CelestialObjects.Find(id);
+
+            if(objectToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            objectToUpdate.Name = name;
+
+            _context.CelestialObjects.Update(objectToUpdate);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var objectsToDelete = _context.CelestialObjects.Where(c => c.Id == id || c.OrbitedObjectId == id).ToList();
+
+            if(!objectsToDelete.Any())
+            {
+                return NotFound();
+            }
+
+            _context.CelestialObjects.RemoveRange(objectsToDelete);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
     }
 }
